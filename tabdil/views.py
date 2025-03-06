0ddaa1924356944b2e28c3576070bee3c5663c39
MA82F <fahimi13823182@gmail.com>
2025-03-06 13:22:55 +0330

    first

---
+++ tabdil/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+
+class tabdil(APIView):
+    def post(self,request):
+        value = request.data.get("value")
+        unit = request.data.get("unit")
+        value = value/1000
+        return Response({
+            "value":value
+        })
+        
+        
+# Create your views here.
