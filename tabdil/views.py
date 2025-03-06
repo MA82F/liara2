f9aa595de5c1ee75ffb59a816792360a1cc914fa
MA82F <fahimi13823182@gmail.com>
2025-03-06 13:30:01 +0330

    second

--- tabdil/views.py
+++ tabdil/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 
 class tabdil(APIView):
