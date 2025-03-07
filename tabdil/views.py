0449fd3f38ee60053479fb0ead759f8593281440
MA82F <fahimi13823182@gmail.com>
2025-03-07 14:15:04 +0330

    new feature added

--- tabdil/views.py
+++ tabdil/views.py
@@ -6,11 +6,43 @@ from rest_framework.views import APIView
 class tabdil(APIView):
     def post(self,request):
         value = request.data.get("value")
-        unit = request.data.get("unit")
-        value = value/1000
+        first_unit = request.data.get("first_unit")
+        second_unit = request.data.get("second_unit")
+        match first_unit:
+            case "ton":
+                match second_unit:
+                    case "kg":
+                        value = value*1000
+                    case "g":
+                        value = value*1000000
+                    case "mg":
+                        value = value*1000000000
+            case "kg":
+                match second_unit:
+                    case "g":
+                        value = value*1000
+                    case "mg":
+                        value = value*1000000
+                    case "ton":
+                        value = value/1000
+            case "g":
+                match second_unit:
+                    case "kg":
+                        value = value/1000
+                    case "mg":
+                        value = value*1000
+                    case "ton":
+                        value = value/1000000
+            case "mg":
+                match second_unit:
+                    case "kg":
+                        value = value/1000000
+                    case "g":
+                        value = value/1000
+                    case "ton":
+                        value = value/1000000000
+                
         return Response({
             "value":value
         })
-        
-        
 # Create your views here.
